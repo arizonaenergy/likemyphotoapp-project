d32d61f4445019ce831182f3ad721d3339a7c0bd
arizonaenergy <example@example.com>
2023-02-04 19:13:43 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import LikePhotoApp from "./LikePhotoApp";
+
+function App() {
+  return (
+    <div>
+      <LikePhotoApp />
+    </div>
+  );
+}
+
+export default App;
